9d38f177a6a821898760b13b07fa1476d2c33d07
Vincent Chuck <vincent.chuck7@gmail.com>
2022-10-17 18:18:57 +0800

    Commit exercise 3.22 and submit all exercises

--- index.js
+++ index.js
@@ -9,28 +9,29 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-morgan.token('person', (req, res) => {
+morgan.token('person', (req) => {
     return req.method === 'POST'
-    ? JSON.stringify(
-        {
-            name: req.body.name,
-            number: req.body.number
-        }
-    )
-    : null
+        ? JSON.stringify(
+            {
+                name: req.body.name,
+                number: req.body.number
+            }
+        )
+        : null
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person', )) 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person', ))
 
 app.post('/api/persons', (req, res, next) => {
     const { name, number } = req.body
-    
+
     const person = new Person({ name, number })
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
-    .catch(error => next(error))
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/', (req, res) => {
@@ -59,55 +60,57 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
-      .then(person => {
-          if (person) {
-            res.json(person)
-          } else {
-            res.status(404).end()
-          }
-      })
-      .catch(error => next(error))
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
     const { name, number } = req.body
 
-    Person.findByIdAndUpdate(
-        req.params.id, 
-        { name, number }, 
-        { new: true , runValidators: true, context: 'queary'})
-      .then(updatedPerson => {
-        res.json(updatedPerson)
-      })
-      .catch(error => next(error))
+    Person
+        .findByIdAndUpdate(
+            req.params.id,
+            { name, number },
+            { new: true , runValidators: true, context: 'queary' })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-      .then(result => {
-        console.log(`${result.name} was deleted`)
-        res.status(204).end()
-      })
-      .catch(error => next(error))
+    Person
+        .findByIdAndRemove(req.params.id)
+        .then(result => {
+            console.log(`${result.name} was deleted`)
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+    res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
+    console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message})
-  }
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
 
-  next(error)
+    next(error)
 }
 
 app.use(errorHandler)
